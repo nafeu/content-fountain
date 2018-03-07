8d0abfabf67698a9b12e32784e92e503212008f9
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-03-06 19:12:48 -0500

    Add logic to generate tag output based on selection

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -13,16 +13,29 @@ angular.module('myApp.home', ['ngRoute'])
 
   new ClipboardJS('.btn');
 
+  var MAX_TAGS = 30;
+
   $scope.caption = "";
   $scope.tags = "";
   $scope.tagData = [];
   $scope.showConnections = false;
+  $scope.selectedTopics = [];
 
   $scope.tokens = {
     google: storageService.get('googleToken'),
     trello: storageService.get('trelloToken')
   }
   $scope.sheetId = storageService.get('sheetId');
+
+  $scope.toggleTopic = function(topic) {
+    var index = $scope.selectedTopics.indexOf(topic);
+    if (index !== -1) {
+      $scope.selectedTopics.splice(index, 1);
+    } else {
+      $scope.selectedTopics.push(topic);
+    }
+    $scope.updateTags();
+  }
 
   $scope.save = function(key, value) {
     storageService.set(key, value);
@@ -46,23 +59,19 @@ angular.module('myApp.home', ['ngRoute'])
     });
   }
 
-  $scope.insertTags = function(key) {
-    $scope.tagData.forEach(function(collection){
-      if (collection.category === key) {
-        collection.tags.forEach(function(tag){
-          $scope.tags += "#" + tag + " ";
-        })
-        return;
-      }
-    })
-  }
+  $scope.updateTags = function() {
+    var numTags = Math.floor(MAX_TAGS / $scope.selectedTopics.length);
 
-  $scope.insertCaption = function() {
-    $scope.caption = "Here is a test caption";
-  }
-
-  $scope.insertContact = function() {
-    $scope.contact = "Check out more work by @person";
+    $scope.tags = "";
+    $scope.selectedTopics.forEach(function(topic){
+      $scope.tagData.forEach(function(collection){
+        if (collection.category === topic) {
+          for (var i = 0; i < numTags; i++) {
+            $scope.tags += "#" + collection.tags[i] + " ";
+          }
+        }
+      });
+    });
   }
 
 }]);
