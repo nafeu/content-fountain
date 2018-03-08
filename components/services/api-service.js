305d56443727000584e81bdf63fca5836537d497
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-03-07 19:55:29 -0500

    Add topic and root differentiation in tags

--- components/services/api-service.js
+++ components/services/api-service.js
@@ -3,7 +3,7 @@
 app.service('apiService', function($http) {
   this.getTagData = function(token, sheetId) {
     var googleSheetsApi = "https://sheets.googleapis.com/v4/spreadsheets/";
-    var cellValues = "/values/A2%3AB";
+    var cellValues = "/values/A2%3AC";
     var key = "?key=" + token;
     var requestUrl = googleSheetsApi + sheetId + cellValues + key;
     return $http.get(requestUrl);
