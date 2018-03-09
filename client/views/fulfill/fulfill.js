b72329db434167dfc2c46052173cdf38ab6c45d8
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-03-09 14:44:03 -0500

    Fix bugs and encode json into desc for card creation

--- client/views/fulfill/fulfill.js
+++ client/views/fulfill/fulfill.js
@@ -11,18 +11,29 @@ angular.module('myApp.fulfill', ['ngRoute'])
 
 .controller('FulfillCtrl', ['$scope', 'apiService', 'storageService', function($scope, apiService, storageService) {
 
-    apiService.getCardsFromList(authorizeTrelloRequest({
-      "idList": storageService.get('listId')
-    }))
-      .then(function(res){
-        console.log(res.data);
-        $scope.contentQueue = [];
-        res.data.forEach(function(item){
-          $scope.contentQueue.push({
-            "name": item.name,
-            "url": item.url
-          })
+  $scope.contentQueue = [];
+
+  $scope.connections = {
+    googleApiKey: storageService.get('googleApiKey'),
+    trelloApiKey: storageService.get('trelloApiKey'),
+    trelloOauthToken: storageService.get('trelloOauthToken'),
+    sheetUrl: storageService.get('sheetUrl'),
+    boardUrl: storageService.get('boardUrl'),
+    listId: storageService.get('listId')
+  }
+
+  apiService.getCardsFromList(authorizeTrelloRequest({
+    "idList": $scope.connections.listId
+  }))
+    .then(function(res){
+      console.log(res.data);
+      $scope.contentQueue = [];
+      res.data.forEach(function(item){
+        $scope.contentQueue.push({
+          "name": item.name,
+          "url": item.url
         })
       })
+    })
 
 }]);
