a09caec71d5a41f84d861b69869a1e81f56d7962
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-03-09 20:26:31 -0500

    Implement fulfillment actions

--- views/fulfill/fulfill.js
+++ views/fulfill/fulfill.js
@@ -11,7 +11,8 @@ angular.module('myApp.fulfill', ['ngRoute'])
 
 .controller('FulfillCtrl', ['$scope', 'apiService', 'storageService', function($scope, apiService, storageService) {
 
-  $scope.contentQueue = [];
+  $scope.queuedIdeas = [];
+  $scope.finalizedIdeas = [];
 
   $scope.connections = {
     googleApiKey: storageService.get('googleApiKey'),
@@ -19,28 +20,55 @@ angular.module('myApp.fulfill', ['ngRoute'])
     trelloOauthToken: storageService.get('trelloOauthToken'),
     sheetUrl: storageService.get('sheetUrl'),
     boardUrl: storageService.get('boardUrl'),
-    listId: storageService.get('listId')
+    listIds: {
+      ideas: storageService.get('listId'),
+      actions: "",
+      finalized: "",
+      completed: "",
+    }
   }
 
   $scope.loadCards = function() {
     apiService.getCardsFromList($scope.authorizeTrelloRequest({
-      "idList": $scope.connections.listId
+      "idList": $scope.connections.listIds.ideas
     }))
       .then(function(res){
-        $scope.contentQueue = [];
-        res.data.forEach(function(item){
-          var decodedData = JSON.parse(item.desc);
-          var queuedContent = {
-            "name": item.name,
-            "url": item.url
+        $scope.queuedIdeas = [];
+        res.data.forEach(function(card){
+          var decodedData = JSON.parse(card.desc);
+          var queuedIdea = {
+            "name": card.name,
+            "url": card.url,
+            "id": card.id,
           }
           if (decodedData.caption) {
-            queuedContent.caption = decodedData.caption;
+            queuedIdea.caption = decodedData.caption;
           }
           if (decodedData.tags) {
-            queuedContent.tags = decodedData.tags;
+            queuedIdea.tags = decodedData.tags;
           }
-          $scope.contentQueue.push(queuedContent)
+          $scope.queuedIdeas.push(queuedIdea)
+        })
+      })
+    apiService.getCardsFromList($scope.authorizeTrelloRequest({
+      "idList": $scope.connections.listIds.finalized
+    }))
+      .then(function(res){
+        $scope.finalizedIdeas = [];
+        res.data.forEach(function(card){
+          var decodedData = JSON.parse(card.desc);
+          var finalizedIdea = {
+            "name": card.name,
+            "url": card.url,
+            "id": card.id,
+          }
+          if (decodedData.caption) {
+            finalizedIdea.caption = decodedData.caption;
+          }
+          if (decodedData.tags) {
+            finalizedIdea.tags = decodedData.tags;
+          }
+          $scope.finalizedIdeas.push(finalizedIdea);
         })
       })
   }
@@ -52,6 +80,90 @@ angular.module('myApp.fulfill', ['ngRoute'])
     }, obj)
   }
 
-  $scope.loadCards();
+  $scope.complete = function(card) {
+    apiService.moveCard($scope.authorizeTrelloRequest({
+      "idCard": card.id,
+      "value": $scope.connections.listIds.completed
+    })).then(function(res){
+      var index = $scope.finalizedIdeas.indexOf(card);
+      if (index > -1) {
+        $scope.finalizedIdeas.splice(index, 1);
+      }
+    }, function(err){
+      alert(JSON.stringify(err));
+    });
+  }
+
+  $scope.finalize = function(card) {
+    apiService.moveCard($scope.authorizeTrelloRequest({
+      "idCard": card.id,
+      "value": $scope.connections.listIds.finalized
+    })).then(function(res){
+      var index = $scope.queuedIdeas.indexOf(card);
+      if (index > -1) {
+        $scope.queuedIdeas.splice(index, 1);
+      }
+      $scope.finalizedIdeas.push(card);
+    }, function(err){
+      alert(JSON.stringify(err));
+    });
+  }
+
+  $scope.copyCaption = function(item) {
+
+  }
+
+  $scope.copyTags = function(item) {
+
+  }
+
+  $scope.remove = function(card) {
+    apiService.deleteCard($scope.authorizeTrelloRequest({
+      "idCard": card.id
+    })).then(function(res){
+      if ($scope.queuedIdeas.indexOf(card) > -1) {
+        $scope.queuedIdeas.splice($scope.queuedIdeas.indexOf(card), 1);
+      }
+      if ($scope.finalizedIdeas.indexOf(card) > -1) {
+        $scope.finalizedIdeas.splice($scope.finalizedIdeas.indexOf(card), 1);
+      }
+    }, function(err){
+      alert(JSON.stringify(err));
+    });
+  }
+
+  $scope.loadData = function() {
+    apiService.getBoardLists($scope.authorizeTrelloRequest({
+      "boardUrl": $scope.connections.boardUrl,
+    }))
+      .then(function(res, err){
+        res.data.forEach(function(list){
+          var listName = list.name.toLowerCase()
+          switch(listName) {
+            case "actions":
+              $scope.connections.listIds.actions = list.id;
+              console.log(list.name);
+              break;
+            case "ideas":
+              $scope.connections.listIds.ideas = list.id;
+              console.log(list.name);
+              break;
+            case "finalized":
+              $scope.connections.listIds.finalized = list.id;
+              console.log(list.name);
+              break;
+            case "completed":
+              $scope.connections.listIds.completed = list.id;
+              console.log(list.name);
+              break;
+            default:
+              break;
+          }
+        })
+        $scope.loadCards();
+      })
+  }
+
+  $scope.loadData()
 
 }]);
